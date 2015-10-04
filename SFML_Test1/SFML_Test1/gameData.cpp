448cad945f3f2837fd21e622d8f6b2fd1fa7faa2
sith0726 <macbook@macbooks-MacBook-Air.local>
2015-10-03 23:37:53 -0700

    Network test 5

---
+++ SFML_Test1/SFML_Test1/gameData.cpp
@@ -0,0 +1,26 @@
+#include "gameData.h"
+
+GameData::GameData()
+{
+    //when start...
+    //set the game_state to introduction
+    game_state = intro;
+    //add 1 player, the one that runs this program
+    Player you;
+    player.push_back(you);
+}
+
+void GameData::InsertPlayer(Player& newPlayer)
+{
+    //if the number of players is less than 4, add 1 player
+    if(player.size() < 4)
+    {
+        player.push_back(newPlayer);
+    }
+}
+
+void GameData::RemovePlayer(const int& index)
+{
+    //remove that player from the list(for networking, remember to disconnect the socket.)
+    //...
+}
